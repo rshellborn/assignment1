607a20a7a9dab58a1484b092ec15ac5bd2647602
rshellborn <rachel@shellborn.com>
2017-03-26 15:58:28 -0700

    removing dummy data

--- application/models/Parts.php
+++ application/models/Parts.php
@@ -10,21 +10,6 @@
 
 class Parts extends CI_Model {
 
-    // note: having parts for a complete robot is going to be rare in the next iteration. 
-    var $data = array(
-        array('id' => '1', 'partCode' => 'a1', 'plant' => 'HeadOffice', 'caCode' => 'b7c247',
-            'amount' => '12', 'image' => 'a1.jpeg'),
-        array('id' => '2', 'partCode' => 'a2', 'plant' => 'HeadOffice', 'caCode' => 'fb853c',
-            'amount' => '12', 'image' => 'a2.jpeg'),
-        array('id' => '3', 'partCode' => 'a3', 'plant' => 'HeadOffice', 'caCode' => '7e1b6a',
-            'amount' => '12', 'image' => 'a3.jpeg'),
-        array('id' => '6', 'partCode' => 'b3', 'plant' => 'HeadOffice', 'caCode' => 'fb8656',
-            'amount' => '12', 'image' => 'b3.jpeg'),
-        array('id' => '11', 'partCode' => 'm2', 'plant' => 'HeadOffice', 'caCode' => '5e5a0f',
-            'amount' => '12', 'image' => 'm2.jpeg')
-
-        );
-
     // Constructor
     public function __construct()
     {
