48040282a8cd491c5f5ba7d7b0cfce93c990159d
rshellborn <rachel@shellborn.com>
2017-03-26 19:30:31 -0700

    adding user role dropdown and storing user role in session variable

--- application/views/template.php
+++ application/views/template.php
@@ -10,6 +10,69 @@
         <link rel="stylesheet" type="text/css" media="all" href="/css/bootstrap-3.3.7-dist/css/bootstrap.css" />
         <link rel="stylesheet" type="text/css" media="all" href="/css/lightbox.css" />
         <link rel="stylesheet" type="text/css" media="all" href="/css/style.css" />
+
+        <style>
+            .dropbtn {
+                background-color: #000;
+                color: white;
+                padding: 16px;
+                font-size: 16px;
+                border: none;
+                cursor: pointer;
+            }
+
+            .dropbtn:hover, .dropbtn:focus {
+                background-color: #3e8e41;
+            }
+
+            .dropdown {
+                position: relative;
+                display: inline-block;
+            }
+
+            .dropdown-content {
+                display: none;
+                position: absolute;
+                background-color: #f9f9f9;
+                min-width: 160px;
+                overflow: auto;
+                box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.2);
+                z-index: 1;
+            }
+
+            .dropdown-content a {
+                color: black;
+                padding: 12px 16px;
+                text-decoration: none;
+                display: block;
+            }
+
+            .dropdown a:hover {background-color: #f1f1f1}
+
+            .show {display:block;}
+        </style>
+        <script>
+            /* When the user clicks on the button,
+             toggle between hiding and showing the dropdown content */
+            function myFunction() {
+                document.getElementById("myDropdown").classList.toggle("show");
+            }
+
+            // Close the dropdown if the user clicks outside of it
+            window.onclick = function(event) {
+                if (!event.target.matches('.dropbtn')) {
+
+                    var dropdowns = document.getElementsByClassName("dropdown-content");
+                    var i;
+                    for (i = 0; i < dropdowns.length; i++) {
+                        var openDropdown = dropdowns[i];
+                        if (openDropdown.classList.contains('show')) {
+                            openDropdown.classList.remove('show');
+                        }
+                    }
+                }
+            }
+        </script>
     </head>
     <body>
         <div id="wrapper">
@@ -21,10 +84,15 @@
                         <li><a href="/parts">Parts</a></li>
                         <li><a href="/assembly">Assembly</a></li>
                         <li><a href="/history">History</a></li>
-                        <ul class="dropdown-menu" role="menu" aria-labelledby="dLabel">
-                          <li><a href="/roles/actor/Guest">Guest</a></li>
-                          <li><a href="/roles/actor/Owner">Owner</a></li>
-                        </ul>
+                        <li>
+                            <div class="dropdown">
+                                <button onclick="myFunction()" class="dropbtn">Role</button>
+                                <div id="myDropdown" class="dropdown-content">
+                                    <a href="/roles/actor/Worker">Worker</a>
+                                    <a href="/roles/actor/Guest">Guest</a>
+                                </div>
+                            </div>
+                        </li>
                     </ul>
                 </span>
             </div>
